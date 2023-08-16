06a194a02c45728893b65e8ae1f6b1eab60433e8
issiac.torrero <itorrero@sdgecontractor.com>
2023-08-16 14:41:12 -0600

    Completed first half of test case examples

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if((typeof num1 !== 'number' || num1 < 0) || (typeof num2 !== 'number' || num2 < 0)) return 'ERROR';
+    let sum = 0;
+    for(let i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
